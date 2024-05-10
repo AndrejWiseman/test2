1ecba27ebaba7d48482e609ffd47eab2c74521d6
Andrej <533737@gmail.com>
2024-05-10 14:38:17 +0200

    3

--- test2/settings.py
+++ test2/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = os.getenv('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-qw8v&arc594z_qwr^j+xwdv9$@d%brc*tmj7c+n-3*p9i^y6#-'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
